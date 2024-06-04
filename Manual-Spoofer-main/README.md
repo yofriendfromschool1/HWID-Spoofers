c36aef34c60c364c63d305c3cd565879ca3bd3f9
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-06-04 15:42:19 -0700

    Add files via upload

---
+++ Manual-Spoofer-main/README.md
@@ -0,0 +1,17 @@
+# Manual Amidewin
+Manual Amidewin (HP & DELL Not Supported)
+---------------------------------------------------------------------------------
+# Instructions
+1) Download The Repo.
+2) Extract Manual Spoof.rar.
+3) Right Click On "spoof.bat" & replace [ Put Your File Directory Here! ] With Your File Directory.
+4) Do Not Remove cd from "cd [ Put Your File Directory Here! ]"
+---------------------------------------------------------------------------------
+# Spoofing
+1) Right Click On "spoof.bat" And Change These Serials **Must Do After Every Ban**.
+2) Change /CS & /BS Serials In "spoof.bat".
+3) Run "spoof.bat" As Administrator .
+4) After Running "spoof.bat" Open CMD As Administrator.
+5) In CMD Type "net stop winmgmt" Then "Y" To The Following Prompt.
+---------------------------------------------------------------------------------
+# Password Is 123
---
+++ Manual-Spoofer-main/spoof.bat.bat
@@ -0,0 +1,24 @@
+cd [ Put Your File Directory Here! ]
+@echo off
+cls
+
+spoof.EXE /SU "AUTO" /BS "281638849284512"
+spoof.EXE /BM "Gigabyte Technology Co., Ltd." /BP "Z690 UD DDR4" /BV "1.0" /BT "Default string" /BLC "Default string" /CM "Gigabyte Technology Co., Ltd." /CT "03" /CV "Default String" /CS "483928172849283" /CSK "SKU" /PSN "To Be Filled By O.E.M." /PAT "To Be Filled By O.E.M."  /PPN "To Be Filled By O.E.M." /OS 1 "Default String" 
+
+
+spoof.EXE /IVN "American Megatrends Inc."
+spoof.EXE /IV "LLC. F23"
+spoof.EXE /ID "09/03/2023"
+spoof.EXE /SF "To be filled by O.E.M."
+spoof.EXE /BT "Default string"
+spoof.EXE /BLC "Default string"
+spoof.EXE /CM "Gigabyte Technology Co., Ltd."
+spoof.EXE /CV "Default string"
+spoof.EXE /CS "483928172849383"
+spoof.EXE /CA "Default string"
+spoof.EXE /CSK "Default string"
+spoof.EXE /SM "To Be Filled By O.E.M."
+spoof.EXE /SP "Z690 UD DDR4"
+spoof.EXE /SV "System Version"
+spoof.EXE /SS "Default string"
+spoof.EXE /SK "SKU"
