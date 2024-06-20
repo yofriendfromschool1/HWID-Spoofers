61fe37d4cba3cc408918ba4d08d708f11029c3e5
yofriendfromschool1 <153390719+yofriendfromschool1@users.noreply.github.com>
2024-06-20 02:44:35 -0700

    Add files via upload

---
+++ spoof n clean/spoofers/serial_checker.bat
@@ -0,0 +1,19 @@
+ECHO OFF
+TITLE HWID Checker
+ECHO **********************************
+Color 0F
+ECHO **********************************
+:start
+cls
+wmic diskdrive get model, serialnumber
+ECHO CPU 
+wmic cpu get serialnumber
+ECHO BIOS
+wmic bios get serialnumber
+ECHO Motherboard
+wmic baseboard get serialnumber
+ECHO smBIOS UUID
+wmic path win32_computersystemproduct get uuid
+getmac
+echo Press any key to get go back to spoofer menu
+pause>nul
